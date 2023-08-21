8e57df5140b764507c88d4d3e6145e392de3b765
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-21 18:24:55 +0100

    remove

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { register, login } from '../Controller/auth.js';
-
-const authRouter = express.Router();
-authRouter.post('/register', register);
-authRouter.post('/login', login);
-
-export default authRouter;
--- server/routes/user.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { createAdmin } from '../Controller/user.js';
-const userRouter = express.Router();
-
-userRouter.get('/admin', createAdmin);
-
-export default userRouter;
