18ebc25e66ffadb6632351a17dae455eddd5c84b
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-01 18:45:17 +0100

    Update home.js

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import Slider from '../Components/image-slider';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section>
+      <section>
+        <Slider />
+      </section>
+      <section className="hero-sec2">
+        <div className="hero-beastSeller">
+          <img src="Images/banner/best-img.jpg" alt="best-seller" />
+          <div className="best-text">
+            <h1>Best Seller</h1>
+            <button className="btn-best">Shop Now</button>
+          </div>
+        </div>
+        <div className="hero-beastSeller">
+          <img src="Images/banner/images.jpeg" alt="best-seller2" />
+          <div className="best-text">
+            <h1>New Arrivals</h1>
+            <button className="btn-best">Shop Now</button>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 }
 
 export default Home;
