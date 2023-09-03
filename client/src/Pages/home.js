a39199e1fe42e93f7744093325f96522f871cb2f
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-03 04:18:41 +0100

    Update home.js

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import Card from '@mui/material/Card';
 import Slider from '../Components/image-slider';
 
 function Home() {
   return (
-    <section>
+    <section className="slider-container">
       <section>
         <Slider />
       </section>
       <section className="hero-sec2">
-        <div className="hero-beastSeller">
-          <img src="Images/banner/best-img.jpg" alt="best-seller" />
-          <div className="best-text">
-            <h1>Best Seller</h1>
-            <button className="btn-best">Shop Now</button>
+        <Card sx={{ height: 500 }} className="card">
+          <div className="hero-beastSeller">
+            <img src="Images/banner/best-img.jpg" alt="best-seller" />
+            <div className="best-text">
+              <h1>Best Seller</h1>
+              <button className="btn-best">Shop Now</button>
+            </div>
           </div>
-        </div>
-        <div className="hero-beastSeller">
-          <img src="Images/banner/images.jpeg" alt="best-seller2" />
-          <div className="best-text">
-            <h1>New Arrivals</h1>
-            <button className="btn-best">Shop Now</button>
+        </Card>
+        <Card sx={{ height: 500 }} className="card">
+          <div className="hero-beastSeller">
+            <img src="Images/banner/images.jpeg" alt="best-seller2" />
+            <div className="best-text">
+              <h1>New Arrivals</h1>
+              <button className="btn-best">Shop Now</button>
+            </div>
           </div>
-        </div>
+        </Card>
       </section>
     </section>
   );
