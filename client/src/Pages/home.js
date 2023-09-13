93b10687b2c5104b74ac9f8deda6c198a684a322
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-13 08:45:07 +0100

    Update home.js

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import Slider from '../Components/image-slider';
-import { Button, IconButton } from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import Rating from '@mui/material/Rating';
-import Stack from '@mui/material/Stack'
-import data from '../data';
-
+import BestSeller from '../Components/bestSeller';
 function Home() {
-  const product = data.product;
-  const bestSeller = product.filter((item) => item.bestSeller === true);
-  console.log(bestSeller);
   return (
     <section className="slider-container">
       <section>
@@ -37,37 +29,11 @@ function Home() {
         </Card>
       </section>
       {/*beastSeller */}
-      <section className="best-seller container-fluid">
-        <h1 className='text-center mt-5'>Best Seller</h1>
-        <h5 className='text-center mb-5'>TOP PRODUCTS OF THIS WEEK</h5>
-        <div className="row-container">
-          {bestSeller.map((item) => (
-            <div className="card-container" key={item.id}>
-              <div className="card">
-                <img src={item.image} alt={item.name} />
-                <div className="card-body">
-                  <span>{item.brand }</span>
-                  <h5 className='card-title'>{item.name}</h5>
-                  <span className='text-center'>{item.price}FCFA</span>
-                  <Stack spacing={2}>
-                    <div  className='rating'>
-                    <Rating name="half-rating-read" defaultValue={item.rating} precision={0.5} readOnly />
-                      <span>{item.numReview} Reviews</span>
-                      </div>
-                  </Stack><br/>
-                  <div className='bttons'>
-                  <Button variant="contained" color="primary" >
-                 ADD TO CARD
-                    </Button>
-                    <IconButton className='whistlist'>
-                      <FavoriteIcon className='whistlist'/>
-                    </IconButton>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
+      <BestSeller />
+      <section className='trending container-fliud'>
+        <h1>Trending</h1>
+        <h5>TOP WISHES OF THIS WEEK
+</h5>
       </section>
     </section>
   );
