428f58e892f1432cfec3a7eb2222e2b42ac56f17
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-13 01:36:50 +0100

    Update home.js

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import Slider from '../Components/image-slider';
+import { Button, IconButton } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack'
 import data from '../data';
 
 function Home() {
@@ -13,7 +17,7 @@ function Home() {
         <Slider />
       </section>
       <section className="hero-sec2">
-        <Card sx={{ height: 500 }} className="card">
+        <Card sx={{ height: 500 }} className="best-card">
           <div className="hero-beastSeller">
             <img src="Images/banner/best-img.png" alt="best-seller" />
             <div className="best-text">
@@ -22,7 +26,7 @@ function Home() {
             </div>
           </div>
         </Card>
-        <Card sx={{ height: 500 }} className="card">
+        <Card sx={{ height: 500 }} className="best-card">
           <div className="hero-beastSeller">
             <img src="Images/banner/images.png" alt="best-seller2" />
             <div className="best-text">
@@ -34,14 +38,31 @@ function Home() {
       </section>
       {/*beastSeller */}
       <section className="best-seller container-fluid">
-        <h1>Best Seller</h1>
-        <div className="row">
+        <h1 className='text-center mt-5'>Best Seller</h1>
+        <h5 className='text-center mb-5'>TOP PRODUCTS OF THIS WEEK</h5>
+        <div className="row-container">
           {bestSeller.map((item) => (
-            <div className="col" key={item.id}>
+            <div className="card-container" key={item.id}>
               <div className="card">
                 <img src={item.image} alt={item.name} />
                 <div className="card-body">
-                  <h5>{item.name}</h5>
+                  <span>{item.brand }</span>
+                  <h5 className='card-title'>{item.name}</h5>
+                  <span className='text-center'>{item.price}FCFA</span>
+                  <Stack spacing={2}>
+                    <div  className='rating'>
+                    <Rating name="half-rating-read" defaultValue={item.rating} precision={0.5} readOnly />
+                      <span>{item.numReview} Reviews</span>
+                      </div>
+                  </Stack><br/>
+                  <div className='bttons'>
+                  <Button variant="contained" color="primary" >
+                 ADD TO CARD
+                    </Button>
+                    <IconButton className='whistlist'>
+                      <FavoriteIcon className='whistlist'/>
+                    </IconButton>
+                  </div>
                 </div>
               </div>
             </div>
