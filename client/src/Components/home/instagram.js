7c2660731bec3eea34fbe40aa1539ca0847b03c0
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-15 17:29:54 +0100

    add instagram post

---
+++ client/src/Components/home/instagram.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Instagram() {
+  return (
+    <section className="container-fluid">
+      <h1 className="text-center fw-bold mt-5 ">@ Follows Us On</h1>
+      <h6 className="text-center mb-5 ">INSTAGRAM</h6>
+      <section className="instagram-container">
+        <img
+          src="Images/banner/instagram.png"
+          alt="instagram"
+          height={200}
+          width={300}
+        />
+        <img
+          src="Images/banner/instagram.png"
+          alt="instagram"
+          height={200}
+          width={300}
+        />
+        <img
+          src="Images/banner/instagram.png"
+          alt="instagram"
+          height={200}
+          width={300}
+        />
+        <img
+          src="Images/banner/instagram.png"
+          alt="instagram"
+          height={200}
+          width={300}
+        />
+        <img
+          src="Images/banner/instagram.png"
+          alt="instagram"
+          height={200}
+          width={300}
+        />
+        <img
+          src="Images/banner/instagram.png"
+          alt="instagram"
+          height={200}
+          width={300}
+        />
+      </section>
+    </section>
+  );
+}
+
+export default Instagram;
