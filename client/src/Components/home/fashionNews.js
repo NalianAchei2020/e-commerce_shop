337bae613c9c3371ffcbc7ea91419e5590264316
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-15 13:53:31 +0100

    Create fashionNews.js

---
+++ client/src/Components/home/fashionNews.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
+import AssignmentReturnIcon from '@mui/icons-material/AssignmentReturn';
+
+function FashionNews() {
+  return (
+    <section className="container-fluid">
+      <h1 className="text-center fw-bold mt-5 ">Fashion News</h1>
+      <h6 className="text-center mb-5">
+        The latest fashion news, celebrity style, fashion week updates
+      </h6>
+      <section className="new-main-container">
+        <Link to="/" className="link">
+          <div className="new-container">
+            <div className="new-image">
+              <img src="Images/women/high-heels.jpg" alt="heels" />
+            </div>
+            <div className="new-text">
+              <p className="date  mt-3">Feb 25, 2023</p>
+              <p className="fw-bold">The post-pandemic fashion</p>
+              <p>
+                The denim resurgence is the result of the long, secretive days
+                that people have to stay indoors. The era of spor...
+              </p>
+              <p>
+                {' '}
+                <Link to="/" className="link discover">
+                  {' '}
+                  Discover
+                </Link>
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/" className="link">
+          <div className="new-container">
+            <div className="new-image">
+              <img src="Images/women/high-heels.jpg" alt="heels" />
+            </div>
+            <div className="new-text">
+              <p className="date  mt-3">Feb 25, 2023</p>
+              <p className="fw-bold">The New Fashion Arrivals We’re Excited</p>
+              <p>
+                The denim resurgence is the result of the long, secretive days
+                that people have to stay indoors. The era of spor...
+              </p>
+              <p>
+                {' '}
+                <Link to="/" className="link discover">
+                  {' '}
+                  Discover
+                </Link>
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link to="/" className="link">
+          <div className="new-container">
+            <div className="new-image">
+              <img src="Images/women/high-heels.jpg" alt="heels" />
+            </div>
+            <div className="new-text">
+              <p className="date mt-3">Feb 25, 2023</p>
+              <p className="fw-bold">The post-pandemic fashion</p>
+              <p>
+                The denim resurgence is the result of the long, secretive days
+                that people have to stay indoors. The era of spor...
+              </p>
+              <p>
+                {' '}
+                <Link to="/" className="link discover">
+                  Discover
+                </Link>
+              </p>
+            </div>
+          </div>
+        </Link>
+      </section>
+      <section className="support-container">
+        <Link to="/" className="link">
+          <div className="support">
+            <LocalShippingIcon />
+            <div>
+              <h5>Return Shipping</h5>
+              <span>On order over $150</span>
+            </div>
+          </div>
+        </Link>
+        <Link to="/" className="link">
+          <div className="support">
+            <SupportAgentIcon />
+            <div>
+              <h5>Support 24/7</h5>
+              <span>Contact us 24 hours a day, 7 days a week</span>
+            </div>
+          </div>
+        </Link>
+        <Link to="/" className="link">
+          <div className="support">
+            <AssignmentReturnIcon />
+            <div>
+              <h5>30 days return</h5>
+              <span>
+                Simply return it within 30 days of purchase for an exhange
+              </span>
+            </div>
+          </div>
+        </Link>
+      </section>
+    </section>
+  );
+}
+
+export default FashionNews;
