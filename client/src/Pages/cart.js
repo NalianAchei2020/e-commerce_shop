0b5a191bc190464d4ce8ae67083e4cf21a8c5a81
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-18 09:57:00 +0100

    Update cart.js

---
+++ client/src/Pages/cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import data from '../data';
+
+function Cart() {
+  const product = data.product;
+  return (
+    <section className="container-fluid cart-container">
+      <p>cart</p>
+    </section>
+  );
+}
+
+export default Cart;
