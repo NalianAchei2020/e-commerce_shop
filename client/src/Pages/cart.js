c8cbf9b88322acbef3e61f49295a326f141ddb6f
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-18 16:16:24 +0100

    Update cart.js

--- client/src/Pages/cart.js
+++ client/src/Pages/cart.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import data from '../../../server/data';
-
+import { useSelector } from 'react-redux';
 function Cart() {
-  const product = data.product;
+  const { cart } = useSelector((state) => state.product);
   return (
     <section className="container-fluid cart-container">
       <p>cart</p>
+      {cart.map((item) => (
+        <div key={item.id}>
+          <p>{item.name}</p>
+          <p>{item.price}</p>
+        </div>
+      ))}
     </section>
   );
 }
