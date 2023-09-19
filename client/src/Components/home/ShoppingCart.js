ec23e546f44934b78686ec3db1963b6c7894ca52
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-19 15:02:20 +0100

    Update ShoppingCart.js

--- client/src/Components/home/ShoppingCart.js
+++ client/src/Components/home/ShoppingCart.js
@@ -1,9 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ClearIcon from '@mui/icons-material/Clear';
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import CloseIcon from '@mui/icons-material/Close';
+import Confetti from 'react-confetti';
+import ProgressBar from '@ramonak/react-progress-bar';
 import {
   addToCart,
   removeFromCart,
@@ -13,6 +20,11 @@ import {
 function ShoppingCart({ popup, handleClosePopup }) {
   const { cart } = useSelector((state) => state.product);
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(true);
+  //free shipping
+  const freeShippingValue = 200000;
+  const currentValue = cart.reduce((a, c) => a + c.price * c.quantity, 0);
+  const balance = freeShippingValue - currentValue;
 
   const popupRef = useRef(null);
 
@@ -58,11 +70,12 @@ function ShoppingCart({ popup, handleClosePopup }) {
                 <div className="previewCart-details">
                   <h5>{item.name}</h5>
                   <span>{item.price} FCFA </span>
-                  <ul className="d-flex flex-row gap-5 previewCart-list">
+                  <ul className="d-flex flex-row gap-3 justify-content-center align-center previewCart-list">
                     <li>
                       <RemoveIcon
                         onClick={() => handleDeleteItem(item)}
                         className="previewCart-icon"
+                        style={{ fontSize: '16px' }}
                       />
                     </li>
                     <li className="qty">{item.quantity}</li>
@@ -70,6 +83,7 @@ function ShoppingCart({ popup, handleClosePopup }) {
                       <AddIcon
                         onClick={() => handleAddToCart(item)}
                         className="previewCart-icon"
+                        style={{ fontSize: '16px' }}
                       />
                     </li>
                   </ul>
@@ -83,16 +97,81 @@ function ShoppingCart({ popup, handleClosePopup }) {
               </div>
             ))
           : null}
+        <div className="mt-4">
+          {currentValue >= freeShippingValue ? (
+            <Box sx={{ width: '100%' }}>
+              <Collapse in={open}>
+                <Confetti
+                  width={window.innerWidth}
+                  height={window.innerHeight}
+                />
+                <Alert
+                  action={
+                    <IconButton
+                      aria-label="close"
+                      color="inherit"
+                      size="small"
+                      onClick={() => {
+                        setOpen(false);
+                      }}
+                    >
+                      <CloseIcon fontSize="inherit" />
+                    </IconButton>
+                  }
+                  sx={{ mb: 2 }}
+                >
+                  Congratulations you got free shipping!
+                </Alert>
+              </Collapse>
+            </Box>
+          ) : (
+            <div>
+              <div class="progress">
+                <div
+                  className="progress-bar"
+                  role="progressbar"
+                  style={{
+                    width: `${(currentValue / freeShippingValue) * 100}%`,
+                    backgroundColor:
+                      currentValue < freeShippingValue ? '#1976d2' : 'red',
+                    color: '#fff',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    padding: '1em',
+                  }}
+                  aria-valuenow={currentValue}
+                  aria-valuemin="0"
+                  aria-valuemax={freeShippingValue}
+                >
+                  <span> {balance} FCFA more to get free shipping</span>
+                </div>
+              </div>
+              <ProgressBar
+                completed={80}
+                customLabel={`${balance} FCFA more to get free shipping`}
+                labelSize="10px"
+                labelAlignment="center"
+                width="100%"
+              />
+            </div>
+          )}
+        </div>
         <ul className="d-flex flex-row justify-content-between mt-4">
           <li>
             {' '}
             <h5>Sub Total</h5>
           </li>
-          <li className="fs-20">
+          <li className="cart-price">
             {cart.reduce((a, c) => a + c.quantity, 0)} Items:{' '}
             {cart.reduce((a, c) => a + c.price * c.quantity, 0)} FCFA
           </li>
         </ul>
+        <div className="btn-cart d-flex flex-column gap-4 mt-4 ">
+          <button className="checkout-btn">Checkout Now</button>
+          <button className="viewCart-btn btn-best ">View Cart</button>
+        </div>
       </section>
     </section>
   );
