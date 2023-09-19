4f6e3284282eb894e8f2685176f5d3fc0b1fcfb5
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-19 10:24:47 +0100

    Update ShoppingCart.js

--- client/src/Components/home/ShoppingCart.js
+++ client/src/Components/home/ShoppingCart.js
@@ -1,15 +1,21 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useRef } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ClearIcon from '@mui/icons-material/Clear';
 import {
   addToCart,
   removeFromCart,
   removeItem,
 } from '../../redux/productSlice';
-function ShoppingCart() {
+
+function ShoppingCart({ popup, handleClosePopup }) {
   const { cart } = useSelector((state) => state.product);
   const dispatch = useDispatch();
+
+  const popupRef = useRef(null);
+
   const handleAddToCart = (item) => {
     dispatch(addToCart(item));
   };
@@ -19,9 +25,29 @@ function ShoppingCart() {
   const handleDeleteItem2 = (item) => {
     dispatch(removeItem(item));
   };
+  //close the popup container when the cursor is clicked outside of it
+  /*useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        handleClosePopup();
+      }
+    };
+
+    window.addEventListener('click', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, []);*/
   return (
-    <section className="perviewCart-container">
-      <h3>Shopping Cart</h3>
+    <section
+      className={popup ? 'perviewCart-container' : 'null'}
+      ref={popupRef}
+    >
+      <div className="previewCart-title d-flex flex-row justify-content-between gap-4">
+        <h3>Shopping Cart</h3>
+        <ClearIcon className="previewCart-icon" onClick={handleClosePopup} />
+      </div>
       <section>
         {cart.length
           ? cart.map((item) => (
@@ -32,22 +58,41 @@ function ShoppingCart() {
                 <div className="previewCart-details">
                   <h5>{item.name}</h5>
                   <span>{item.price} FCFA </span>
-                  <ul className="d-flex flex-row gap-4">
+                  <ul className="d-flex flex-row gap-5 previewCart-list">
                     <li>
-                      <RemoveIcon onClick={() => handleDeleteItem(item)} />
+                      <RemoveIcon
+                        onClick={() => handleDeleteItem(item)}
+                        className="previewCart-icon"
+                      />
                     </li>
-                    <li>{item.quantity}</li>
+                    <li className="qty">{item.quantity}</li>
                     <li>
-                      <AddIcon onClick={() => handleAddToCart(item)} />
+                      <AddIcon
+                        onClick={() => handleAddToCart(item)}
+                        className="previewCart-icon"
+                      />
                     </li>
                   </ul>
                   <div className="delete">
-                    <DeleteIcon onClick={() => handleDeleteItem2(item)} />
+                    <DeleteIcon
+                      onClick={() => handleDeleteItem2(item)}
+                      className="previewCart-icon"
+                    />
                   </div>
                 </div>
               </div>
             ))
           : null}
+        <ul className="d-flex flex-row justify-content-between mt-4">
+          <li>
+            {' '}
+            <h5>Sub Total</h5>
+          </li>
+          <li className="fs-20">
+            {cart.reduce((a, c) => a + c.quantity, 0)} Items:{' '}
+            {cart.reduce((a, c) => a + c.price * c.quantity, 0)} FCFA
+          </li>
+        </ul>
       </section>
     </section>
   );
