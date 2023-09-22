0174468e3abbeb0d389473070a11e68957a2554e
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-22 10:51:33 +0100

    Update carousel.js

--- client/src/Components/home/carousel.js
+++ client/src/Components/home/carousel.js
@@ -15,48 +15,54 @@ function CarouselProduct({
   alt,
   numReview,
   rating,
+  item,
+  handleAddToCart,
 }) {
   return (
     <div className="card-container">
-      <Link to="/" className="product-link">
-        <div className="card">
-          <div className="card-image">
+      <div className="card">
+        <div className="card-image">
+          <Link to={`/product/${name}`} className="product-link">
             <img src={image} alt={alt} />
-          </div>
-          <div className="card-body">
-            <span>{brand}</span>
-            <h5 className="card-title">{name}</h5>
-            <span className="text-center">{price} FCFA</span>
-            <Stack spacing={2}>
-              <div className="rating">
-                <Rating
-                  name="size-large"
-                  defaultValue={rating}
-                  precision={0.5}
-                  readOnly
-                />
-                <span>{numReview}</span>
-              </div>
-            </Stack>
-            <br />
-            <div className="bttons">
-              <Button variant="contained" color="primary">
-                ADD TO CARD
-              </Button>
-              <Tooltip title="Wishlist" placement="bottom">
-                <IconButton className="whistlist">
-                  <FavoriteIcon className="whistlist" />
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="Quick View" placement="bottom">
-                <IconButton className="whistlist">
-                  <VisibilityIcon className="whistlist" />
-                </IconButton>
-              </Tooltip>
+          </Link>
+        </div>
+        <div className="card-body">
+          <span>{brand}</span>
+          <h5 className="card-title">{name}</h5>
+          <span className="text-center">{price} FCFA</span>
+          <Stack spacing={2}>
+            <div className="rating">
+              <Rating
+                name="size-large"
+                defaultValue={rating}
+                precision={0.5}
+                readOnly
+              />
+              <span>{numReview}</span>
             </div>
+          </Stack>
+          <br />
+          <div className="bttons">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleAddToCart(item)}
+            >
+              ADD TO CARD
+            </Button>
+            <Tooltip title="Wishlist" placement="bottom">
+              <IconButton className="whistlist">
+                <FavoriteIcon className="whistlist" />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Quick View" placement="bottom">
+              <IconButton className="whistlist">
+                <VisibilityIcon className="whistlist" />
+              </IconButton>
+            </Tooltip>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
