142b642662fd622ab312f4a5f00fb6869d3eb584
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-22 14:31:11 +0100

    Update product.js

--- client/src/Pages/product.js
+++ client/src/Pages/product.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { IconButton } from '@mui/material';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+import { addToCart, removeFromCart } from '../redux/productSlice';
 
 function Product() {
+  const dispatch = useDispatch();
   const [usersCount, setUsersCount] = useState(0);
   const { slug } = useParams();
 
@@ -11,6 +17,13 @@ function Product() {
   const selectedProduct = product.find((item) => item.name === slug);
   console.log(selectedProduct);
   const productID = selectedProduct ? selectedProduct.id : 'error';
+
+  const handleAddToCart = (item) => {
+    dispatch(addToCart(item));
+  };
+  const handleDeleteItem = (item) => {
+    dispatch(removeFromCart(item));
+  };
 
   useEffect(() => {
     const viewProduct = async () => {
@@ -25,7 +38,9 @@ function Product() {
 
     const leaveProduct = async () => {
       try {
-        await axios.post('http://localhost:8000/api/views/product/leave');
+        await axios.post('http://localhost:8000/api/views/product/leave', {
+          id: productID,
+        });
       } catch (error) {
         console.log(error);
       }
@@ -50,6 +65,10 @@ function Product() {
     };
   }, [productID]);
 
+  if (!selectedProduct) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <section>
       <h1>Product</h1>
@@ -59,7 +78,33 @@ function Product() {
         </div>
         <div className="des-text">
           <h2>{selectedProduct.name}</h2>
-          <h3>views: {usersCount}</h3>
+          <div className="d-flex flex-row gap-4">
+            <IconButton>
+              <VisibilityIcon
+                sx={{ color: 'black', fontSize: '2rem', fontWeight: '800' }}
+              />
+            </IconButton>
+            <h6 className="mt-3">{usersCount} views</h6>
+          </div>
+          <div className="d-flex flex-row gap-4">
+            <ul className="d-flex flex-row gap-3 justify-content-center align-center previewCart-list">
+              <li>
+                <RemoveIcon
+                  onClick={() => handleDeleteItem(selectedProduct)}
+                  className="previewCart-icon"
+                  style={{ fontSize: '16px' }}
+                />
+              </li>
+              <li className="qty">{selectedProduct.quantity}</li>
+              <li>
+                <AddIcon
+                  onClick={() => handleAddToCart(selectedProduct)}
+                  className="previewCart-icon"
+                  style={{ fontSize: '16px' }}
+                />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
