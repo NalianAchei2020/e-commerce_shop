eddd818ad8fe698125f13c4e075ea25e08dfdcce
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-22 11:38:28 +0100

    Update product.js

--- client/src/Pages/product.js
+++ client/src/Pages/product.js
@@ -1,22 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 function Product() {
+  const [usersCount, setUsersCount] = useState(0);
   const { slug } = useParams();
 
   const { product } = useSelector((state) => state.product);
   const selectedProduct = product.find((item) => item.name === slug);
+  console.log(selectedProduct);
+  const productID = selectedProduct ? selectedProduct.id : 'error';
+
+  useEffect(() => {
+    const viewProduct = async () => {
+      try {
+        await axios.post('http://localhost:8000/api/views/product/view', {
+          id: productID,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const leaveProduct = async () => {
+      try {
+        await axios.post('http://localhost:8000/api/views/product/leave');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const getCount = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:8000/api/views/product/count'
+        );
+        setUsersCount(response.data.count);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    viewProduct();
+    getCount();
+
+    return () => {
+      leaveProduct();
+    };
+  }, [productID]);
 
   return (
     <section>
       <h1>Product</h1>
       <div className="description-container">
         <div className="description-image">
-          <img src={selectedProduct.image} alt="desImage" />
+          <img src={selectedProduct.image} alt={selectedProduct.name} />
         </div>
         <div className="des-text">
           <h2>{selectedProduct.name}</h2>
+          <h3>views: {usersCount}</h3>
         </div>
       </div>
     </section>
