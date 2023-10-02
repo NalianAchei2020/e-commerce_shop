fad6dde34af3cf18f21278d1b18f6b0415376964
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-02 13:35:22 +0100

    Update shop.js

--- client/src/Pages/shop.js
+++ client/src/Pages/shop.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import Divider from '@mui/material/Divider';
+import BestSeller from '../Components/shop/bestseller';
 
 const Shop = () => {
   return (
@@ -19,38 +21,91 @@ const Shop = () => {
       </div>
       <section className="container-fluid shop-container">
         <aside>
-          <ul className="d-flex flex-row justify-content-between">
-            <li>
-              <h5>Collection</h5>
-            </li>
-            <li>
-              <ExpandLessIcon />
-            </li>
-          </ul>
-          <ul className="aside-list">
-            <li className="d-flex flex-row justify-content-between">
-              <spn>Best Seller</spn>
-              <span>05</span>
-            </li>
-            <li className="d-flex flex-row justify-content-between">
-              <spn>Trending</spn>
-              <span>05</span>
-            </li>
-            <li className="d-flex flex-row justify-content-between">
-              <spn>Men</spn>
-              <span>05</span>
-            </li>
-            <li className="d-flex flex-row justify-content-between">
-              <spn>New Arrivals</spn>
-              <span>05</span>
-            </li>
-            <li className="d-flex flex-row justify-content-between">
-              <spn>Women</spn>
-              <span>05</span>
-            </li>
-          </ul>
+          <div>
+            <ul className="d-flex flex-row justify-content-between">
+              <li>
+                <h5>Collection</h5>
+              </li>
+              <li>
+                <ExpandLessIcon />
+              </li>
+            </ul>
+            <ul className="aside-list">
+              <li className="d-flex flex-row justify-content-between">
+                <spn>Best Seller</spn>
+                <span>05</span>
+              </li>
+              <li className="d-flex flex-row justify-content-between">
+                <spn>Trending</spn>
+                <span>05</span>
+              </li>
+              <li className="d-flex flex-row justify-content-between">
+                <spn>Men</spn>
+                <span>05</span>
+              </li>
+              <li className="d-flex flex-row justify-content-between">
+                <spn>New Arrivals</spn>
+                <span>05</span>
+              </li>
+              <li className="d-flex flex-row justify-content-between">
+                <spn>Women</spn>
+                <span>05</span>
+              </li>
+            </ul>
+          </div>
+          <Divider sx={{ backgroundColor: 'ActiveBorder' }} />
+          <div>
+            <ul className="d-flex flex-row justify-content-between mt-5">
+              <li>
+                <h5>Availability</h5>
+              </li>
+              <li>
+                <ExpandLessIcon />
+              </li>
+            </ul>
+            <ul className="aside-list">
+              <li className="d-flex flex-row justify-content-between">
+                <spn>In Stock</spn>
+                <span>05</span>
+              </li>
+              <li className="d-flex flex-row justify-content-between">
+                <spn>Out of Stock</spn>
+                <span>05</span>
+              </li>
+            </ul>
+          </div>
+          <Divider sx={{ backgroundColor: 'ActiveBorder' }} />
+          <div>
+            <ul className="d-flex flex-row justify-content-between mt-5">
+              <li>
+                <h5>Price</h5>
+              </li>
+              <li>
+                <ExpandLessIcon />
+              </li>
+            </ul>
+          </div>
+          <Divider sx={{ backgroundColor: 'ActiveBorder' }} />
+          <div>
+            <ul className="d-flex flex-row justify-content-between mt-5">
+              <li>
+                <h5>Brand</h5>
+              </li>
+              <li>
+                <ExpandLessIcon />
+              </li>
+            </ul>
+            <ul className="d-flex flex-row justify-content-between mt-3">
+              <li>Gucci</li>
+              <li>Nike</li>
+              <li>Addidas</li>
+            </ul>
+          </div>
+          <Divider sx={{ backgroundColor: 'ActiveBorder' }} />
         </aside>
-        <main></main>
+        <main>
+          <BestSeller />
+        </main>
       </section>
     </section>
   );
