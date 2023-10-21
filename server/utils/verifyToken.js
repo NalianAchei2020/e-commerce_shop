b73f0208caa53e055dfe6f71eb10d4846e3a9f09
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-21 12:15:36 +0100

    changes id to _id

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -3,7 +3,7 @@ import { createError } from './error.js';
 import config from '../config.js';
 
 export const generateToken = (user) => {
-  return jwt.sign({ id: user._id, isAdmin: user.isAdmin }, config.JWT_SECRET, {
+  return jwt.sign({ _id: user._id, isAdmin: user.isAdmin }, config.JWT_SECRET, {
     expiresIn: '24h',
   });
 };
