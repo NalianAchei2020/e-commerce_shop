c940f01f77574380dedfb7045d74368d9ae224ad
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-10-23 14:41:23 +0100

    add payment

--- client/src/Components/checkout/paypal.js
+++ client/src/Components/checkout/paypal.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PayPalButtons } from '@paypal/react-paypal-js';
+import { useSelector } from 'react-redux';
+import { paidOrder } from '../../hooks/getpaypal';
 
-function Paypal() {
+function Paypal({ total }) {
+  const { usersOders } = useSelector((state) => state.product);
+  const [isPaid, setIsPaid] = useState(false);
+  const [errMessage, setErrMessage] = useState('');
+  console.log(usersOders);
+
+  const handleApprove = (orderId) => {
+    const payment = paidOrder(orderId);
+    if (payment) {
+      setIsPaid(true);
+    } else {
+      setErrMessage(
+        'Your payment was processed successfully. However, we are unable to fulfill your purchase. Please contact us at support.example@gmail.com for asssistance'
+      );
+    }
+    if (isPaid) {
+      setErrMessage('Payment successful');
+    }
+  };
   return (
     <PayPalButtons
       style={{ color: 'blue', layout: 'horizontal', tagline: false }}
-      createOrder={(data, action) => {
-        return action.order.create({
+      createOrder={(data, actions) => {
+        return actions.order.create({
           purchase_units: [
             {
               description: 'test',
-              amount: { value: '10.00' },
+              amount: {
+                currency_code: 'USD',
+                value: total,
+              },
             },
           ],
         });
+      }}
+      onApprove={async (data, actions) => {
+        const order = await actions.order.capture();
+        console.log('order:', order);
+
+        handleApprove(data.orderID);
       }}
     />
   );
