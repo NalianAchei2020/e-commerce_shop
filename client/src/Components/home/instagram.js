3c21d1a81f94b9ed8febee1033dd5fad965013b5
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-11-14 13:30:14 +0100

    add instagram post

--- client/src/Components/home/instagram.js
+++ client/src/Components/home/instagram.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Instagram() {
   return (
@@ -6,42 +7,54 @@ function Instagram() {
       <h2 className="text-center fw-bold mt-5 ">@ Follows Us On</h2>
       <h6 className="text-center mb-5 ">INSTAGRAM</h6>
       <section className="instagram-container">
-        <img
-          src="Images/banner/instagram.png"
-          alt="instagram"
-          height={200}
-          width={300}
-        />
-        <img
-          src="Images/banner/instagram.png"
-          alt="instagram"
-          height={200}
-          width={300}
-        />
-        <img
-          src="Images/banner/instagram.png"
-          alt="instagram"
-          height={200}
-          width={300}
-        />
-        <img
-          src="Images/banner/instagram.png"
-          alt="instagram"
-          height={200}
-          width={300}
-        />
-        <img
-          src="Images/banner/instagram.png"
-          alt="instagram"
-          height={200}
-          width={300}
-        />
-        <img
-          src="Images/banner/instagram.png"
-          alt="instagram"
-          height={200}
-          width={300}
-        />
+        <Link to="instagram.com">
+          <img
+            src="Images/banner/insta1.jpeg"
+            alt="instagram"
+            height={200}
+            width={300}
+          />
+        </Link>
+        <Link to="instagram.com">
+          <img
+            src="Images/banner/insta2.jpeg"
+            alt="instagram"
+            height={200}
+            width={300}
+          />
+        </Link>
+        <Link to="instagram.com">
+          <img
+            src="Images/banner/insta3.jpeg"
+            alt="instagram"
+            height={200}
+            width={300}
+          />
+        </Link>
+        <Link to="instagram.com">
+          <img
+            src="Images/banner/insta6.jpeg"
+            alt="instagram"
+            height={200}
+            width={300}
+          />
+        </Link>
+        <Link to="instagram.com">
+          <img
+            src="Images/banner/insta4.jpg"
+            alt="instagram"
+            height={200}
+            width={300}
+          />
+        </Link>
+        <Link to="instagram.com">
+          <img
+            src="Images/banner/insta5.jpg"
+            alt="instagram"
+            height={200}
+            width={300}
+          />
+        </Link>
       </section>
     </section>
   );
