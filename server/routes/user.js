69132412b5a335bb97420b60767da263ab5c2077
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-12-14 09:22:28 +0100

    update acces token

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createAdmin, updateUser } from '../Controller/user.js';
+import { verifiedToken, verifyUser } from '../utils/verifyToken.js';
 const userRouter = express.Router();
 
 userRouter.get('/admin', createAdmin);
-userRouter.put('/:id', updateUser);
+userRouter.put('/:id', verifiedToken, verifyUser, updateUser);
 
 export default userRouter;
