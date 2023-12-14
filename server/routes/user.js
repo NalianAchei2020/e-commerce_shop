b3f44f72c9482badffdcd3ba5753d606f8dcb57a
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-12-14 02:53:50 +0100

    add user update

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,6 @@ import { verifiedToken, verifyUser } from '../utils/verifyToken.js';
 const userRouter = express.Router();
 
 userRouter.get('/admin', createAdmin);
-userRouter.put('/:id', verifiedToken, verifyUser, updateUser);
+userRouter.put('/:id', updateUser);
 
 export default userRouter;
